7ff3953d54b37d77b7af99946cc7b8df6d34f77c
iMaeGoo <3870622-imaegoo@users.noreply.gitlab.com>
2021-12-30 13:11:20 +0000

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,11 +1,9 @@
 <?php
 
 // Allow from any origin
-if (isset($_SERVER['HTTP_ORIGIN'])) {
-    header("Access-Control-Allow-Origin: https://www.imaegoo.com");
-    header('Access-Control-Allow-Credentials: true');
-    header('Access-Control-Max-Age: 86400');    // cache for 1 day
-}
+header("Access-Control-Allow-Origin: https://www.imaegoo.com");
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Max-Age: 86400');    // cache for 1 day
 // Access-Control headers are received during OPTIONS requests
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
@@ -19,7 +17,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 $file= __DIR__ . '/..'.$_SERVER["PHP_SELF"];
 
 if (file_exists($file)) {
-    ob_end_clean();
     ob_start();
     // 打开文件
     $handler = fopen($file, 'rb');
